359962117e8cd26fbaa32fa2ba434ba36c779739
ritaaa_gc <rita.gonzales.coene@gmail.com>
2023-12-22 20:41:21 -0300

    commit to-do-list

--- src/server.js
+++ src/server.js
@@ -1,38 +1,100 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.use(express.static("public"));
 app.use(express.json());
+// app.use(express.static(path.join(__dirname, 'public')));
+
 
 let tasks = [
-  { id: 1, description: "hacer las compras", complete: false },
+  { id: 1, description: "hacer las compras", complete: true },
   { id: 2, description: "estudiar NodeJs", complete: false },
 ];
 
+app.get("/", (req, res) => {
+  // res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+
+});
+
 app.get("/tasks", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-  res.json({ tasks })
-});
-
-app.put("/tasks/:id", (req, res) => {
-  const taskId = parseInt(req.params.id);
-  const task = tasks.find((task) => task.id === taskId);
-
-  if (task) {
-    task.complete = true;
-    res.json(task);
-  } else {
-    res.status(404).json({ error: "Task no found." });
-  }
-});
-
-app.delete("/tasks/:id", (req, res) => {
-  const taskId = parseInt(req.params.id);
-  tasks = tasks.filter((task) => task.id !== taskId);
-  res.json({ message: "Task delete successfully." });
+  res.json(tasks)
 });
 
 app.listen(port, () => {
   console.log(`This server listens on http://localhost:${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.put("/tasks/:id", (req, res) => {
+//   const taskId = parseInt(req.params.id);
+//   const task = tasks.find((task) => task.id === taskId);
+
+//   if (task) {
+//     task.complete = true;
+//     res.json(task);
+//   } else {
+//     res.status(404).json({ error: "Task no found." });
+//   }
+// });
+
+// app.delete("/tasks/:id", (req, res) => {
+//   const taskId = parseInt(req.params.id);
+//   tasks = tasks.filter((task) => task.id !== taskId);
+//   res.json({ message: "Task delete successfully." });
+// });
